2db60342541a4a76d5cbefe066f3d1081c7f022a
Mahendran.R <mahendranr211@gmail.com>
2026-08-13 20:33:48 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor=0;
+        int n=nums.length;
+        for(int i=1;i<=n;i++)
+        {
+            xor^=i;
+        }
+        for(int num:nums)
+        {
+            xor^=num;
+        }
+        return xor;
+    }
+}
